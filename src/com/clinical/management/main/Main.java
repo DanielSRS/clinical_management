eadfceb9ed12f1256eabf95b96439d0cef2428d8
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2021-11-28 21:18:47 +0000

    :sparkles: Add minimize and maximize buttons

--- src/com/clinical/management/main/Main.java
+++ src/com/clinical/management/main/Main.java
@@ -39,13 +39,16 @@ public class Main extends Application {
         stage.show();
     }
 
-    private Parent buildWindow(Stage stage) {
+    private BorderPane buildWindow(Stage stage) {
         BorderPane windowContainer = new BorderPane();  // Container da janela
-        windowContainer.setPrefSize(900, 500); // Tamanho inicial da janela
+        windowContainer.setPrefSize(1175, 651); // Tamanho inicial da janela
+        BorderPane top = new BorderPane(); //Top
 
         Node titleBarContainer = buildTitleBar(stage);
+        top.setCenter(titleBarContainer);
+        top.setLeft(stackNavigator.getBackButton());
 
-        windowContainer.setTop(titleBarContainer); // Adicina a barra de titulo na janela
+        windowContainer.setTop(top); // Adicina a barra de titulo na janela
 
         // Estilos da janela
         windowContainer.setId("windowContainer");
@@ -63,15 +66,42 @@ public class Main extends Application {
 
 		Button closeButton = new Button();  // Botão de fechar a janela
         closeButton.setId("closeButton"); //windowCloseIcon
+        
+        
+        /// bottaõ de minimizar
+        Button minimizeButton = new Button();  // Botão de min a janela
+        minimizeButton.setId("minimizeButton"); //windowCloseIcon
+        minimizeButton.setOnAction(e -> this.handleMinimize());
+        Image minimizeIcon = new Image(getClass().getResourceAsStream("../view/images/windowMinimizeIcon.png"));
+        ImageView windowMinimizeIcon = new ImageView(minimizeIcon);
+        windowMinimizeIcon.setPreserveRatio(true);
+        windowMinimizeIcon.fitWidthProperty().set(10);
+        windowMinimizeIcon.fitHeightProperty().set(10);
+        minimizeButton.setGraphic(windowMinimizeIcon);
+        minimizeButton.setPrefSize(45, 35);
+        titleBarContainer.getChildren().add(minimizeButton);
+        
+        Button maxButton = new Button();  // Botão de max a janela
+        maxButton.setId("maxButton"); //windowCloseIcon
+        maxButton.getStyleClass().add("svv");
+        maxButton.setOnAction(e -> this.handleMaximize());
+        Image maximizeIcon = new Image(getClass().getResourceAsStream("../view/images/windowMaximizeIcon.png"));
+        ImageView windowMaximizeIcon = new ImageView(maximizeIcon);
+        windowMaximizeIcon.setPreserveRatio(true);
+        windowMaximizeIcon.fitWidthProperty().set(10);
+        windowMaximizeIcon.fitHeightProperty().set(10);
+        maxButton.setGraphic(windowMaximizeIcon);
+        maxButton.setPrefSize(45, 35);
+        titleBarContainer.getChildren().add(maxButton);
 
-        // imagem do botão
+        // imagem do botão de fechcar
         Image imageOk = new Image(getClass().getResourceAsStream("../view/images/windowCloseIcon.png"));
         ImageView img = new ImageView(imageOk);
         img.setPreserveRatio(true);
         img.fitWidthProperty().set(10);
         img.fitHeightProperty().set(10);
         closeButton.setGraphic(img);
-        closeButton.setPrefSize(35, 35);
+        closeButton.setPrefSize(45, 35);
 
 		closeButton.setOnAction(e -> stage.close());
         titleBarContainer.getChildren().add(closeButton);  // Adiciona o botão de fechar na barra de titulo
@@ -93,7 +123,31 @@ public class Main extends Application {
             }
         });
 		// fim mover tela
+		
+		// double click para window mode
+		titleBarContainer.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			 
+		    @Override
+		    public void handle(MouseEvent click) {
+		 
+		        if (click.getClickCount() == 2) {
+		        	handleMaximize();
+		        }
+		    }
+		});
 
         return titleBarContainer;
+    }
+
+    public void handleMaximize() {
+    	if (this.re != null) {
+    		this.re.switchWindowedMode();
+    	}
+    }
+    
+    public void handleMinimize() {
+    	if (this.re != null) {
+    		this.re.minimize();
+    	}
     }
 }
